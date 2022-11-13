2adfc7b8ead5577999c0810ca663673a2059bafa
sidirasg <georgesidiras@gmail.com>
2022-11-13 12:49:52 +0200

    Main

--- firstApp/urls.py
+++ firstApp/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from .views import first_view
 
 urlpatterns = [
-    path('admin/', first_view.site),
+    path('', first_view),
+
 ]
--- firstApp/views.py
+++ firstApp/views.py
@@ -1,4 +1,5 @@
- from django.http import HttpResponse
+
+from django.http import HttpResponse
 
 # Create your views here.
 def first_view(request):
